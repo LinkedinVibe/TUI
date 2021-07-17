04de089a4d70551e25d20d0d697b5ce91b987067
Zakhar Bozhok <zaharbozhok@gmail.com>
2021-07-17 16:41:01 +0300

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { ITUI } from "./itui";
-import { PartialActions, TUI, TUICommandNotFoundError, TUIError } from "./tui";
+import { TUI, PartialActions,  TUICommandNotFoundError, TUIError } from "./tui";
+
 import { IAction } from "./iaction";
 import { Action, ActionFromPartialError, InvalidParameterError } from "./action";
+
 import { ITerminal } from "./iterminal";
 import { Terminal } from "./terminal";
 
@@ -26,5 +28,7 @@ export {
     InvalidParameterError,
     ITerminal,
     Terminal,
+    IActionCallsProvider,
+    ArgsActionCallsProvider,
     HandleArgs
 }
